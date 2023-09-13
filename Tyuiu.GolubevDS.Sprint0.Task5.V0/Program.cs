613a33574d7144e55cf10739c7e07c0a5877b16f
голубев дима <maska.l.v@mail.ru>
2023-09-13 15:51:38 +0300

    Task5

--- Tyuiu.GolubevDS.Sprint0.Task5.V0/Program.cs
+++ Tyuiu.GolubevDS.Sprint0.Task5.V0/Program.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.GolubevDS.Sprint0.Task5.V0
         {
             Console.WriteLine(DataService.Addition(1, 5));
             Console.WriteLine(DataService.Subtraction(15, 5));
-            Console.WriteLine(DataService.Multiplication(100, 10));
+            Console.WriteLine(DataService.Multiplication(10, 10));
             Console.WriteLine(DataService.Division(1, 0));
 
             Console.ReadKey();
